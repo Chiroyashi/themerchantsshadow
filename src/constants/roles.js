f4f75a46a729177a4f7a1bd75ff7b71ec424fa0c
Chiroyashi <Akbarhakim541@gmail.com>
2026-03-20 20:24:38 +0700

    Game

--- src/constants/roles.js
+++ src/constants/roles.js
@@ -6,7 +6,7 @@ export const TEAM_SERIGALA = [
 export const TEAM_WARGA = [
   { name: "Seer", desc: "Mengetahui role asli 1 pemain setiap malam.", type: "Special" },
   { name: "Guard", desc: "Menjaga 1 pemain agar tidak mati selama 2 malam.", type: "Special" },
-  { name: "Hakim", desc: "Memiliki skill Truth (paksa jujur) dan wajib berkata 'Wahai rakyatku' sebelum memberikan judgement.", type: "Special" },
+  { name: "Hakim", desc: "Memiliki skill Truth (paksa jujur) dan wajib berkata 'WAHAI RAKYATKU' sebelum memberikan keputusan.", type: "Special" },
   { name: "Hunter", desc: "Jika menembak Serigala, ia selamat. Jika salah tembak warga, ia ikut mati.", type: "Special" },
   { name: "Pedagang", desc: "Warga biasa yang menjual dagangan untuk Warlock. Meninggalkan jejak warlock setelah transaksi.", type: "Civilian" }
 ];
